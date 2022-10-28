320b0bc96ce5e41ece9082fc0064903f3aec6f52
Jinxin Lin <JINXIN.LIN07@myhunter.cuny.edu>
2022-10-27 23:49:12 -0400

    finished task D

--- box.cpp
+++ box.cpp
@@ -8,7 +8,7 @@ void box(std::string if_name, std::string of_name)
 	int h, w;
 	readImage(if_name, img, h, w);
 	IMG new_img;
-	std::copy(&img[0][0], &img[0][0] + h * w, &new_img[0][0]);
+	std::copy(&img[0][0], &img[0][0] + MAX_H * MAX_W, &new_img[0][0]);
 	for (int i = h / 4 ; i < h * 3 / 4; i++)
 		for (int j = w / 4; j < w * 3 / 4; j++)
 			new_img[i][j] = 255;
